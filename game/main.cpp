17d74bd0556153785310219a7b05f0f2a2883f4a
zhaolc_bzh <zhaolc_bzh@163.com>
2021-01-02 13:26:55 +0800

    优化

--- game/main.cpp
+++ game/main.cpp
@@ -1,59 +1,56 @@
 #include <iostream>
 #include <fstream>
+#include <memory>
 
-constexpr const char* usage = "usage [n2v/v2n] src dst";
-constexpr size_t step = 4096u * 4096u;
+constexpr const char* usage = "usage:\t[n2v/v2n] src dst\ne.g.\tlumi n2v a.ndf a.mp4\t将a.ndf转换为a.mp4\n\tlumi v2n a.mp4 a.ndf\t将a.mp4转换为a.ndf\n";
 
 int main(int argc, char** argv)
 {
-	if(argc != 4)
+	if (argc != 4)
 	{
 		std::cout << usage << std::endl;
 		return -2;
 	}
 
 	std::ifstream ifs(argv[2], std::ios::binary);
-	if(!ifs.is_open())
+	if (!ifs.is_open())
 	{
 		std::cout << "打开输入文件失败\n";
 		return -4;
 	}
 
 	std::ofstream ofs(argv[3], std::ios::binary);
-	if(!ofs.is_open())
+	if (!ofs.is_open())
 	{
 		std::cout << "打开输出文件失败";
 		return -16;
 	}
 
 
-	if(!strcmp(argv[1], "n2v"))
+	if (!strcmp(argv[1], "n2v"))		// ndf -> mp4
 	{
 		ifs.seekg(2, std::ios::beg);
 	}
-	else if(!strcmp(argv[1], "v2n"))
+	else if (!strcmp(argv[1], "v2n"))		// mp4 -> ndf
 	{
-		char head[2] = { (char)0xFF,(char)0x95 };
+		char head[] = { (char)0xFF, (char)0x95 };
 		ofs.write(head, 2);
 	}
 	else
 	{
-		std::cout << "参数错误\n";
+		std::cout << "参数错误\n" << usage << std::endl;
 		return -32;
 	}
 
 
 	std::cout << argv[2] << " --> " << argv[3] << std::endl;
-	char* buf = new char[step + 1u];
-	while(!ifs.eof())
+	constexpr long buffer_size = 0x4000;
+	auto buffer = std::unique_ptr<char>(new char[buffer_size]);
+	while (!ifs.eof())
 	{
-		ifs.read(buf, step);
-		ofs.write(buf, ifs.gcount());
+		ifs.read(buffer.get(), buffer_size);
+		ofs.write(buffer.get(), ifs.gcount());
 	};
-
-
-	delete[] buf;
-	buf = nullptr;
 
 	ifs.close();
 	ofs.close();
